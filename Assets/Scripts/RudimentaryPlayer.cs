aee6bcc5b9b8b441e8fea9a3c5ebf55eb02222f8
Nelson Luis Moreira da Costa <nelsonlmdcosta@gmail.com>
2025-05-13 14:00:42 +0100

    Rijigged a bunch of files into different locations to facilitate access in the messy project

---
+++ Assets/Scripts/RudimentaryPlayer.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RudimentaryPlayer : MonoBehaviour
+{
+    [SerializeField] private InputActionReference MovementAction;
+    [SerializeField] private InputActionReference JumpAction;
+    [SerializeField] private float MovementSpeed = 2.0f;
+    [SerializeField] private float JumpImpulseForce = 5.0f;
+    [SerializeField] private float DistanceMultiplier = 0.52f;
+    
+    private Rigidbody myRigidbody = null;
+
+    private Vector3 WorldDirection = Vector3.zero;
+    private bool IsJumping = false;
+    private bool IsGrounded = false;
+    
+    private void OnEnable()
+    {
+        MovementAction.action.Enable();
+        JumpAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        MovementAction.action.Disable();
+        JumpAction.action.Disable();
+    }
+
+    private void Awake()
+    {
+        // Three Types Of Actions
+        //MovementAction.action.started += ActionStarted;
+        MovementAction.action.performed += OnLocomtionActionPerformed;
+        //MovementAction.action.canceled += ActionCancelled;
+        
+        JumpAction.action.performed += OnJumpActionPerformed;
+        JumpAction.action.canceled += OnJumpActionCancelled;
+
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        IsGrounded = Physics.Raycast(transform.position, -transform.up, DistanceMultiplier);
+
+        Debug.DrawLine(transform.position, transform.position - transform.up * DistanceMultiplier, Color.red);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 CurrentVelocity = myRigidbody.linearVelocity;
+        myRigidbody.linearVelocity = new Vector3(WorldDirection.x * MovementSpeed, CurrentVelocity.y, WorldDirection.z * MovementSpeed);
+
+        if (IsGrounded && IsJumping)
+        {
+            myRigidbody.AddForce(Vector3.up * JumpImpulseForce, ForceMode.Impulse);
+        }
+    }
+
+    #region InputCallbacks
+
+    private void OnLocomtionActionPerformed(InputAction.CallbackContext CallbackContext)
+    {
+        Vector2 Input = CallbackContext.ReadValue<Vector2>();
+
+        WorldDirection.x = Input.x;
+        WorldDirection.z = Input.y;
+    }
+    
+    private void OnJumpActionPerformed(InputAction.CallbackContext CallbackContext)
+    {
+        IsJumping = true;
+    }
+
+    private void OnJumpActionCancelled(InputAction.CallbackContext CallbackContext)
+    {
+        IsJumping = false;
+    }
+    
+    #endregion
+    
+    
+}
